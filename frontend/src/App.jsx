35423baf620117f60dbed418aa3886544bbc8c34
Andry <andry.sheremetew@gmail.com>
2025-01-18 23:30:30 +0200

    add routes + fonts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,32 @@
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Error from './pages/Error';
+import Home from './pages/Home';
+import About from './pages/About';
+import SingIn from './pages/SingIn';
+import SingUp from './pages/SingUp';
+import Projects from './pages/Projects';
+import Dashboard from './pages/Dashboard';
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='*' element={<Error />} />
+
+        <Route path='/' element={<Home/>} />
+
+        <Route path='/about' element={<About />} />
+        
+        <Route path='/sing-in' element={<SingIn/>} />
+
+        <Route path='/sing-up' element={<SingUp/>} />
+
+        <Route path='/projects' element={<Projects/>} />
+
+        <Route path='/dashboard' element={<Dashboard/>} />
+
+      </Routes>
+    </BrowserRouter>
   )
 }
 
