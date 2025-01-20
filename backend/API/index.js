89cbb8517b28ccd1223b552b120292e593dc1771
Andry <andry.sheremetew@gmail.com>
2025-01-20 15:00:37 +0200

    create test user API route (MVC)

--- backend/API/index.js
+++ backend/API/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js"
 
 const PORT = process.env.PORT || 3000;
 
@@ -18,4 +19,6 @@ const app = express();
 
 app.listen(PORT, ()=> {
   console.log(`server running on port ${PORT}`);
-})
+});
+
+app.use("/api/user", userRouter);
