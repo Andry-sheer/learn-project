9bcb3ba985685ae917cfbc5567c8670007d430be
Andry <andry.sheremetew@gmail.com>
2025-01-20 14:10:02 +0200

    connect to MongoDB + move RDM file + replace gitIgnore + add Dynamic-PORT

--- backend/API/index.js
+++ backend/API/index.js
@@ -1,7 +1,21 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+const PORT = process.env.PORT || 3000;
+
+dotenv.config({ path: '../.env' });
+
+mongoose.connect(process.env.MONGO)
+  .then(()=> { 
+    console.log("MongoDB is connected")
+    })
+    .catch(err => {
+    console.log(err);
+  });
 
 const app = express();
 
-app.listen(3000, ()=> {
-  console.log("server running on port 3000");
+app.listen(PORT, ()=> {
+  console.log(`server running on port ${PORT}`);
 })
